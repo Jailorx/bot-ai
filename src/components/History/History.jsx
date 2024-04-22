dc4ce9f14818e6e5f544192e26e1eb1bdff72a63
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-22 13:46:02 +0530

    improvent in logic of History component  if there is no saved chats

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -20,41 +20,43 @@ const History = () => {
         mb: 1,
       }}
     >
+      <Typography
+        variant="h4"
+        color="primary.main"
+        sx={{
+          fontWeight: "700",
+          padding: "10px",
+          flex: 1,
+        }}
+        mb={2}
+      >
+        History
+      </Typography>
       {chat ? (
-        <>
-          <Typography
-            variant="h4"
-            color="primary.main"
-            sx={{
-              fontWeight: "700",
-              padding: "10px",
-              flex: 1,
-            }}
-            mb={2}
-          >
-            History
-          </Typography>
-          <Grid
-            container
-            direction="column"
-            spacing={1}
-            sx={{
-              backgroundColor: (theme) => theme.palette.secondary.main,
-              borderRadius: "5px",
-              p: 1,
-            }}
-          >
-            {chat.map((message, idx) => (
-              <Grid item key={idx}>
-                <Typography>
-                  <b>{message["type"]}</b> : {message["content"]}
-                </Typography>
-              </Grid>
-            ))}
-          </Grid>
-        </>
+        <Grid
+          container
+          direction="column"
+          spacing={1}
+          sx={{
+            backgroundColor: (theme) => theme.palette.secondary.main,
+            borderRadius: "5px",
+            p: 1,
+          }}
+        >
+          {chat.map((message, idx) => (
+            <Grid item key={idx}>
+              <Typography>
+                <b>{message["type"]}</b> : {message["content"]}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
       ) : (
-        <Grid container></Grid>
+        <Grid container justifyContent="center" alignItems="center">
+          <Typography variant="h4" sx={{ fontWeight: "500" }}>
+            You dont have any saved chats
+          </Typography>
+        </Grid>
       )}
     </Box>
   );
